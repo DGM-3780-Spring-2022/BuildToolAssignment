9e663103c80d556acad6a82d587b7abf1e5688a5
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-03-04 21:22:28 -0700

    Some base stuff for the todo app

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
+function test(){
+    console.log("Testing");
+}
 
+test();
+
+let todo = [
+    {
+        id: 1,
+        item: "Do Homework",
+        complete: false,
+    },
+    {
+        id: 2,
+        item: "Search for Internships",
+        complete: false,
+    },
+    {
+        id: 3,
+        item: "Travel Home",
+        complete: true,
+    },
+]
