f25c52ffe92b7587ed745810f2d691e5dd3ed932
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-03-06 14:08:41 -0700

    The list should be working, but its not

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ function test(){
 
 test();
 
-let todo = [
+let todos = [
     {
         id: 1,
         item: "Do Homework",
@@ -18,6 +18,44 @@ let todo = [
     {
         id: 3,
         item: "Travel Home",
-        complete: true,
+        complete: false,
     },
 ]
+
+const todoList = (todoItems) => {
+    document.querySelector('.todoList')
+    for (let i=0; i<=todoItems.length; i++) {
+    let itemDiv = document.createAttribute('div')
+    itemDiv.class = "newDivision"
+    let statusLabel = document.createElement('input')
+    statusLabel.type = "checkbox";
+    statusLabel.class = false;
+    let idLabel = document.createElement('p')
+    idLabel.textContent = todos.length ++;
+    let itemLabel = document.createElement('p')
+    itemLabel.textContent = todos[i].item;
+    let buttonLabel = document.createElement('button')
+    buttonLabel.textContent = "Edit item"
+    buttonLabel.class = "edit"
+    itemDiv.appendChild(statusLabel)
+    itemDiv.appendChild(idLabel)
+    itemDiv.appendChild(itemLabel)
+    itemDiv.appendChild(buttonLabel)
+    return itemDiv
+    }
+}
+
+todoList(todos);
+
+addTodos.addEventListener('click', () => {
+
+});
+
+const editTodo = () => {
+
+}
+
+const deleteTodos = () => {
+    
+}
+
