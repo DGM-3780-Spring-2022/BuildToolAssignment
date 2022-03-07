7a77c0a52d7870da39e19955c19bcb973bf5c4ad
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-03-06 20:07:22 -0700

    Webpack hopefully working

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,59 @@
-import _ from 'lodash';
+function test(){
+    console.log("Testing");
+}
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+test();
+
+let todos = []
+
+class todo {
+    constructor(name, status) {
+    this.id = todos.length++;
+    this.name = name;
+    this.status = status;
+    }
+}
+
+const addTodos = () => {
+
+addTodos.addEventListener('click', () => {
+
+});
+
+}
+
+const todoList = (todoItems) => {
+    document.querySelector('.todoList')
+    async (todos) => {
+    for (let i=0; i<=todoItems.length; i++) {
+    let itemDiv = document.createAttribute('div')
+    itemDiv.class = "newDivision"
+    let statusLabel = document.createElement('input')
+    statusLabel.type = "checkbox";
+    statusLabel.class = false;
+    let idLabel = document.createElement('p')
+    idLabel.textContent = todos.length ++;
+    let itemLabel = document.createElement('p')
+    itemLabel.textContent = todos[i].item;
+    let buttonLabel = document.createElement('button')
+    buttonLabel.textContent = "Edit item"
+    buttonLabel.class = "edit"
+    itemDiv.appendChild(statusLabel)
+    itemDiv.appendChild(idLabel)
+    itemDiv.appendChild(itemLabel)
+    itemDiv.appendChild(buttonLabel)
+    return itemDiv
+    }
+}
+}
+
+todoList(todos);
+
+const editTodo = () => {
+
+}
+
+const deleteTodos = () => {
+    
+}
+
